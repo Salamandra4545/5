241e27d04a30a772ccea48298a8209ce84b26335
elik <pochta@bk.ru>
2023-06-23 17:13:53 +0200

    first commit

---
+++ 5/Complex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    internal class Complex
+    {
+        private double a;
+        private double b;
+        public double A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public double B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public Complex(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+
+
+        public override string? ToString()
+        {
+            if (B > 0)
+                return A + "+" + B + "i";
+            else if (B < 0)
+                return A + "-" + Math.Abs(B) + "i";
+            return A.ToString();
+        }
+    }
+}
---
+++ 5/Program.cs
@@ -0,0 +1,108 @@
+﻿using _5;
+
+int a, b, c, d;
+try
+{
+    Console.Write("Введите А:");
+    a = int.Parse(Console.ReadLine());
+    Console.Write("Введите B:");
+    b = int.Parse(Console.ReadLine());
+    Console.Write("Введите C:");
+    c = int.Parse(Console.ReadLine());
+    Console.Write("Введите D:");
+    d = int.Parse(Console.ReadLine());
+
+    try
+    {
+        Complex Div = DivComlpex(a, b, c, d);
+        Console.WriteLine(Div);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+    try
+    {
+        Complex Div2 = DivComlpex2(a, b, c, d);
+        Console.WriteLine(Div2);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    try
+    {
+        Complex Div3 = DivComplex3(a, b, c, d);
+        Console.WriteLine(Div3);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    try
+    {
+        Complex Div4 = DivComplex4(a, b, c, d);
+        Console.WriteLine(Div4);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+
+
+}
+catch (FormatException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+catch (ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
+Complex DivComlpex(int a, int b, int c, int d)
+{
+    Complex result = new Complex((a * c + b * d) / (c * c + d * d), (b * c + a * d) / (c * c + d * d));
+    return result;
+}
+
+Complex DivComlpex2(int a, int b, int c, int d)
+{
+    if (c==0 && d == 0)
+    {
+        throw new Exception("Деление на ноль!");
+    }
+    Complex result = new Complex((a * c + b * d) / (c * c + d * d), (b * c + a * d) / (c * c + d * d));
+    return result;
+}
+
+Complex DivComplex3(int a, int b, int c, int d)
+{
+    Complex result = new Complex(0, 0);
+    try
+    {
+        result = new Complex((a * c + b * d) / (c * c + d * d), (b * c + a * d) / (c * c + d * d));
+        return result;
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    return result;
+}
+
+Complex DivComplex4(int a, int b, int c, int d)
+{
+    if (c==0 && d==0)
+    {
+        throw new OurException("Деление на ноль запрещенно!");
+    }
+    Complex result = new Complex((a * c + b * d) / (c * c + d * d), (b * c + a * d) / (c * c + d * d));
+    return result;
+
+}
